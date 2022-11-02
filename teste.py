91a3fa8de62bb8564a73237da434e880899f22bc
Leonardo Pereira de Paiva Pinto <92749112+leonardopereirappp@users.noreply.github.com>
2022-11-02 18:48:59 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,9 +0,0 @@
-from projetosite.models import Usuario, Post, Comentarios
-from projetosite import database
-
-database.create_all()
-
-usuario = Comentarios.query[0]
-print(usuario.corpo)
-print(usuario.titulo)
-
